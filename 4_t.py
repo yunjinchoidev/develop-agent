0fd92765fc3b95104b90a1da756b10b7c8b283d8
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-08-02 02:02:25 +0900

    [Fix] 프롬프트 변경

--- 4_t.py
+++ 4_t.py
@@ -16,18 +16,46 @@ def main():
         verbose=True,
     )
 
+    # python_agent_executor.run(
+    #     """
+    #         develop a add, delete cotrol todo webpage.
+    #         the working file should save in static folder index.html.
+    #         and mapping by fastapi.
+    #         fastapi is already installed in this project.
+    #         you can use uvicorn. uvicorn is already installed.
+    #         you don't have to install anything.
+    #         you should use HTMLResponse to serve your html file and save fastapi file in app.py.
+    #         you should save file.
+    #         you should present me result "localhost:4500" so that I can see the result.
+    #
+    # """
+    # )
+    # python_agent_executor.run(
+    #     """
+    #         develop a add, delete cotrol todo webpage.
+    #         the working file should save in static folder index.html.
+    #         and mapping by fastapi.
+    #         fastapi is already installed in this project.
+    #         you can use uvicorn. uvicorn is already installed.
+    #         you don't have to install anything.
+    #         you should use HTMLResponse to serve your html file and save fastapi file in app.py.
+    #         you should save file.
+    #         you should present me result "localhost:4500" so that I can see the result.
+    #
+    # """
+    # )
+
     python_agent_executor.run(
         """
-            develop a add, delete cotrol todo webpage.
-            the working file should save in static folder index.html. 
-            and mapping by fastapi.
-            fastapi is already installed in this project.
-            you can use uvicorn. uvicorn is already installed.
-            you don't have to install anything.
-            you should use HTMLResponse to serve your html file and save fastapi file in app.py.
-            you should save file.
-            you should present me result "localhost:4500" so that I can see the result.
-                    
+            Develop a add, delete control todo webpage.
+            Save the HTML file in a static folder named 'index.html'.
+            Pay attention to triple-quoted string literals.
+            Map it using FastAPI.
+            There is no need to install anything.
+            Use HTMLResponse to serve your HTML file.
+            Save the FastAPI file as 'app.py'.
+            The available ports you can use are 4500 and 7000.
+            Use uvicorn to launch the FastAPI server, making it directly accessible.    
     """
     )
 
