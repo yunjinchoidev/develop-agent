89c1a717b9d017ddeb1e7baf55f73d058fc49ae2
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-08-04 22:05:07 +0900

    [Refactor] 최종 제출 프롬프트 정리

--- 5_t.py
+++ 5_t.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 from langchain.agents import Tool
 from langchain.tools import PythonREPLTool
-
-# Do this so we can see exactly what's going on under the hood
 import langchain
 
 langchain.debug = True
@@ -13,26 +11,12 @@ langchain.debug = True
 load_dotenv()
 
 
-def serp_search(name: str):
-    """
-    serp search
-    """
-    serpapi = SerpAPIWrapper()
-    result = serpapi.run(f"{name}")
-    print("result: ", result)
-    return result
-
-
 def find_code_search_by_serp(name: str):
-    # Initialize the OpenAI language model
-    # Replace <your_api_key> in openai_api_key="<your_api_key>" with your actual OpenAI key.
+
     llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
 
-    # Initialize the SerpAPIWrapper for search functionality
-    # Replace <your_api_key> in openai_api_key="<your_api_key>" with your actual SerpAPI key.
     search = SerpAPIWrapper()
 
-    # Define a list of tools offered by the agent
     tools = [
         Tool(
             name="Search",
