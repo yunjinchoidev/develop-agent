0a983458d6d7d40b5460bb8ba86ab2401aeb19a0
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-08-04 22:04:36 +0900

    [Refactor] 최종 제출 프롬프트 정리

--- 4_t.py
+++ 4_t.py
@@ -1,8 +1,4 @@
-from dotenv import load_dotenv
-from langchain.agents import AgentType
 from langchain.agents.agent_toolkits import create_python_agent
-from langchain.chat_models import ChatOpenAI
-from langchain.tools import PythonREPLTool
 from langchain.utilities import SerpAPIWrapper
 from langchain.agents import AgentType, initialize_agent
 from dotenv import load_dotenv
@@ -10,57 +6,13 @@ from langchain.chat_models import ChatOpenAI
 from langchain.agents import Tool
 from langchain.tools import PythonREPLTool
 
-# Do this so we can see exactly what's going on under the hood
-import langchain
-
-# langchain.debug = True
-
-
 load_dotenv()
 
 
-def main():
-
-    python_agent_executor = create_python_agent(
-        llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo"),
-        tool=PythonREPLTool(),
-        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-        verbose=True,
-    )
-
-    python_agent_executor.run(
-        """
-            Develop a add, delete control todo webpage.
-            neumorphism style is recommended.
-            Save the HTML file in a static folder named 'index.html'.
-            Pay attention to triple-quoted string literals.
-            Map it using FastAPI.
-            There is no need to install anything.
-            Use HTMLResponse to serve your HTML file.
-            Save the FastAPI file as 'app.py'.
-            The available ports you can use are 4500 and 7000.
-            Use uvicorn to launch the FastAPI server, making it directly accessible.    
-    """
-    )
-
-
-def serp_search(name: str):
-    """
-    serp search
-    """
-    serpapi = SerpAPIWrapper()
-    result = serpapi.run(f"{name}")
-    print("result: ", result)
-    return result
-
-
 def find_code_search_by_serp(name: str):
-    # Initialize the OpenAI language model
-    # Replace <your_api_key> in openai_api_key="<your_api_key>" with your actual OpenAI key.
+
     llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
 
-    # Initialize the SerpAPIWrapper for search functionality
-    # Replace <your_api_key> in openai_api_key="<your_api_key>" with your actual SerpAPI key.
     search = SerpAPIWrapper()
 
     # Define a list of tools offered by the agent
